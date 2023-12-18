d44c13930589f0072444f1736e3e0afd5814c7db
Cameron Smith <cameron.ray.smith@gmail.com>
2023-12-17 22:40:24 -0500

    fix(flake): pin 23.11 Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- flake.nix
+++ flake.nix
@@ -3,7 +3,8 @@
 
   inputs = {
     # Principle inputs (updated by `nix run .#update`)
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
@@ -32,7 +33,7 @@
               imports = [ inputs.self.homeModules.default ];
               home.username = myUserName;
               home.homeDirectory = "/${if pkgs.stdenv.isDarwin then "Users" else "home"}/${myUserName}";
-              home.stateVersion = "22.11";
+              home.stateVersion = "23.11";
             });
           inherit (inputs.nix2container.packages.${system}.nix2container) buildImage;
         in
