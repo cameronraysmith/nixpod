71cbf3916633ffbdb98f35dbf45ebafaafe8c183
Cameron Smith <cameron.ray.smith@gmail.com>
2023-12-17 23:18:04 -0500

    fix(home-manager): pin to 23.11 Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     # Principle inputs (updated by `nix run .#update`)
     # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
