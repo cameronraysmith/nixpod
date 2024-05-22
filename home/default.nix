a53d5f15cb964223c846eaad93d313716695edbb
Cameron Smith <cameron.ray.smith@gmail.com>
2024-05-22 13:16:46 -0400

    docs(home): note home manager options reference Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- home/default.nix
+++ home/default.nix
@@ -3,6 +3,8 @@
   flake = {
     homeModules = {
       default = {
+        # See https://home-manager-options.extranix.com/ for home-manager
+        # options used inside these imported modules.
         imports = [
           ./neovim
           ./starship.nix
