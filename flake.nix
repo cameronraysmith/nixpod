d93a7c4bfbc00d5755eb36eb6e7c69a223f84593
Cameron Smith <cameron.ray.smith@gmail.com>
2024-05-22 13:17:16 -0400

    fix(flake): default to nixos-unstable Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- flake.nix
+++ flake.nix
@@ -3,10 +3,9 @@
 
   inputs = {
     # Principle inputs (updated by `nix run .#update`)
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager";
       inputs = {
         nixpkgs.follows = "nixpkgs";
       };
