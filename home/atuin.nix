5f577bfc06178c2875150c18c4d5d0beec127239
Cameron Smith <cameron.ray.smith@gmail.com>
2024-06-27 12:21:36 -0400

    fix(home): disable atuin auto_sync
    - TODO: enable atuin daemon to be managed by s6
    
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- home/atuin.nix
+++ home/atuin.nix
@@ -3,7 +3,10 @@
     enable = true;
     enableZshIntegration = true;
     settings = {
-      auto_sync = true;
+      # TODO: enable s6 to manage atuin daemon
+      # for compatibility with ceph-filesystem
+      # https://forum.atuin.sh/t/weekly-release-2024-19/317
+      auto_sync = false;
       ctrl_n_shortcuts = false;
       keymap_mode = "vim-insert";
       filter_mode_shell_up_key_binding = "directory";
