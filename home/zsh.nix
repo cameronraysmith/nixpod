16b21a7399dd207be364e9b097a2f0174006f4d6
Cameron Smith <cameron.ray.smith@gmail.com>
2024-07-09 09:13:32 -0400

    fix(terminal): sync PATH for bash and zsh
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- home/zsh.nix
+++ home/zsh.nix
@@ -7,7 +7,7 @@
 
     envExtra = ''
       # Ensure all nix and home-manager installed files are available in PATH.
-      export PATH=/run/wrappers/bin:/run/current-system/sw/bin:/nix/var/nix/profiles/default/bin:$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:/opt/homebrew/bin:$PATH
+      export PATH=/run/wrappers/bin:/run/current-system/sw/bin:/nix/var/nix/profiles/default/bin:$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:$PATH
     '';
 
     initExtraBeforeCompInit = ''
