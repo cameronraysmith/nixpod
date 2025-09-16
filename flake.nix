95afc8de2e7433311d1117a634477db53592cd54
Cameron Smith <cameron.ray.smith@gmail.com>
2025-09-16 19:11:50 -0400

    fix(flake): pin nixos-unified/c23ce31
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       };
     };
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nixos-unified.url = "github:srid/nixos-unified/v0.2.0";
+    nixos-unified.url = "github:srid/nixos-unified/c23ce31c707c3e4b699312ab9c80a4a003e893b1";
     flocken = {
       url = "github:mirkolenz/flocken/v2";
       inputs.nixpkgs.follows = "nixpkgs";
