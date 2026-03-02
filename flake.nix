3cdd2ec263d18bd1ad6d38c63d108835673881ae
Cameron Smith <cameron.ray.smith@gmail.com>
2026-03-02 13:38:37 -0500

    feat(flake): add sops-nix input

--- flake.nix
+++ flake.nix
@@ -29,6 +29,10 @@
       url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nix-index-database = {
       url = "github:nix-community/nix-index-database";
       inputs = {
