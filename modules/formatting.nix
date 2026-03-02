bbe7319f29a716f5d8def24cf7cf0b096d09bf9d
Cameron Smith <cameron.ray.smith@gmail.com>
2026-03-02 12:45:00 -0500

    feat(formatting): replace bare nixfmt with treefmt-nix and git-hooks.nix

--- modules/formatting.nix
+++ modules/formatting.nix
@@ -1,7 +1,18 @@
+{ inputs, ... }:
 {
-  perSystem =
-    { pkgs, ... }:
-    {
-      formatter = pkgs.nixfmt-rfc-style;
+  imports = [
+    inputs.treefmt-nix.flakeModule
+    inputs.git-hooks.flakeModule
+  ];
+
+  perSystem = {
+    treefmt = {
+      projectRootFile = "flake.nix";
+      programs.nixfmt.enable = true;
     };
+
+    pre-commit.settings = {
+      hooks.treefmt.enable = true;
+    };
+  };
 }
