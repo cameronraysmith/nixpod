483ad87d3a6a4c3dca7637535a160efa29d6412c
Cameron Smith <cameron.ray.smith@gmail.com>
2026-03-02 13:07:24 -0500

    feat(dev-shell): wire pre-commit hooks into dev shell
    Add config.pre-commit.devShell to inputsFrom so that nix develop
    installs pre-commit hooks including gitleaks and treefmt.

--- modules/dev-shell.nix
+++ modules/dev-shell.nix
@@ -1,11 +1,14 @@
 {
   perSystem =
-    { pkgs, self', ... }:
+    { pkgs, config, self', ... }:
     {
       apps.default.program = self'.packages.activate-home;
 
       devShells.default = pkgs.mkShell {
         name = "nixpod";
+        inputsFrom = [
+          config.pre-commit.devShell
+        ];
         nativeBuildInputs = with pkgs; [
           act
           just
