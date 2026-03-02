662f991ebfde44e85f8b81f8965052dae1f16950
Cameron Smith <cameron.ray.smith@gmail.com>
2026-03-02 12:58:49 -0500

    feat(formatting): add gitleaks pre-commit hook

--- modules/formatting.nix
+++ modules/formatting.nix
@@ -5,14 +5,23 @@
     inputs.git-hooks.flakeModule
   ];
 
-  perSystem = {
-    treefmt = {
-      projectRootFile = "flake.nix";
-      programs.nixfmt.enable = true;
-    };
+  perSystem =
+    { pkgs, ... }:
+    {
+      treefmt = {
+        projectRootFile = "flake.nix";
+        programs.nixfmt.enable = true;
+      };
 
-    pre-commit.settings = {
-      hooks.treefmt.enable = true;
+      pre-commit.settings = {
+        hooks.treefmt.enable = true;
+        hooks.gitleaks = {
+          enable = true;
+          name = "gitleaks";
+          entry = "${pkgs.gitleaks}/bin/gitleaks protect --staged --verbose --redact";
+          language = "system";
+          pass_filenames = false;
+        };
+      };
     };
-  };
 }
