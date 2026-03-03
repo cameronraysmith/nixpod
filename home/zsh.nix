667ac406227d88b78d635ae395c5b460ce960925
Cameron Smith <cameron.ray.smith@gmail.com>
2026-03-03 13:37:17 -0500

    refactor(home): migrate deprecated zsh initExtra options to initContent

--- home/zsh.nix
+++ home/zsh.nix
@@ -10,46 +10,47 @@
       export PATH=/run/wrappers/bin:/run/current-system/sw/bin:/nix/var/nix/profiles/default/bin:$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:$PATH
     '';
 
-    initExtraBeforeCompInit = ''
-      ZSH_DISABLE_COMPFIX=true
-    '';
+    initContent = lib.mkMerge [
+      (lib.mkOrder 550 ''
+        ZSH_DISABLE_COMPFIX=true
+      '')
+      ''
+        # Shell function to compute the sha256 nix hash of a file from a url.
+        get_nix_hash() {
+          url="$1";
+          nix_hash=$(nix-prefetch-url "$url");
+          nix hash to-sri --type sha256 "$nix_hash";
+        }
 
-    initExtra = ''
-      # Shell function to compute the sha256 nix hash of a file from a url.
-      get_nix_hash() {
-        url="$1";
-        nix_hash=$(nix-prefetch-url "$url");
-        nix hash to-sri --type sha256 "$nix_hash";
-      }
+        # Shell function to check differences between the current branch and the
+        # upstream branch prior to merge.
+        pmc() {
+          export PAGER=cat
+          branch=''${1:-upstream/main}
+          echo 'Commit Summary:'
+          git log HEAD..$branch --oneline
+          echo
+          echo 'Detailed Commit Logs:'
+          git log HEAD..$branch
+          echo
+          echo 'Files Changed (Name Status):'
+          git diff --name-status HEAD...$branch
+          unset PAGER
+        }
 
-      # Shell function to check differences between the current branch and the
-      # upstream branch prior to merge.
-      pmc() {
-        export PAGER=cat
-        branch=''${1:-upstream/main}
-        echo 'Commit Summary:'
-        git log HEAD..$branch --oneline
-        echo
-        echo 'Detailed Commit Logs:'
-        git log HEAD..$branch
-        echo
-        echo 'Files Changed (Name Status):'
-        git diff --name-status HEAD...$branch
-        unset PAGER
-      }
+        # List the active scopes of a GitHub legacy PAT provided as argument.
+        check_github_token_scopes() {
+          if [ -z "$1" ]; then
+            echo "Usage: check_github_token_scopes <your_github_token>"
+            return 1
+          fi
 
-      # List the active scopes of a GitHub legacy PAT provided as argument.
-      check_github_token_scopes() {
-        if [ -z "$1" ]; then
-          echo "Usage: check_github_token_scopes <your_github_token>"
-          return 1
-        fi
+          token=$1
+          curl -sS -f -I -H "Authorization: token $token" https://api.github.com | grep -i x-oauth-scopes
+        }
 
-        token=$1
-        curl -sS -f -I -H "Authorization: token $token" https://api.github.com | grep -i x-oauth-scopes
-      }
-
-    '';
+      ''
+    ];
 
     oh-my-zsh = {
       enable = true;
