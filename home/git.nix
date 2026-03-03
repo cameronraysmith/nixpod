721683547a3957d6f172f036e3ce0df2afa44f80
Cameron Smith <cameron.ray.smith@gmail.com>
2026-03-03 13:36:50 -0500

    refactor(home): migrate deprecated git options to programs.git.settings and programs.delta

--- home/git.nix
+++ home/git.nix
@@ -7,7 +7,7 @@
     #   key = "1234567890ABCDEF";
     #   signByDefault = true;
     # };
-    extraConfig = {
+    settings = {
       core.editor = "nvim";
       credential.helper = "store --file ~/.git-credentials";
       color.ui = true;
@@ -19,52 +19,54 @@
       merge.conflictstyle = "diff3";
       push.autoSetupRemote = true;
       rebase.autoStash = true;
-    };
-    aliases = {
-      a = "add";
-      br = "branch";
-      bra = "branch -a";
-      c = "commit";
-      ca = "commit --amend";
-      can = "commit --amend --no-edit";
-      cavm = "commit -a -v -m";
-      cfg = "config --list";
-      cl = "clone";
-      cm = "commit -m";
-      co = "checkout";
-      cp = "cherry-pick";
-      cpx = "cherry-pick -x";
-      d = "diff";
-      f = "fetch";
-      fo = "fetch origin";
-      fu = "fetch upstream";
-      lease = "push --force-with-lease";
-      lol = "log --graph --decorate --pretty=oneline --abbrev-commit";
-      lola = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
-      pl = "pull";
-      pr = "pull -r";
-      ps = "push";
-      psf = "push -f";
-      rb = "rebase";
-      rbi = "rebase -i";
-      r = "remote";
-      ra = "remote add";
-      rr = "remote rm";
-      rv = "remote -v";
-      rs = "remote show";
-      st = "status";
-      stn = "status -uno";
-    };
-    delta = {
-      enable = true;
-      options = {
-        side-by-side = true;
+      alias = {
+        a = "add";
+        br = "branch";
+        bra = "branch -a";
+        c = "commit";
+        ca = "commit --amend";
+        can = "commit --amend --no-edit";
+        cavm = "commit -a -v -m";
+        cfg = "config --list";
+        cl = "clone";
+        cm = "commit -m";
+        co = "checkout";
+        cp = "cherry-pick";
+        cpx = "cherry-pick -x";
+        d = "diff";
+        f = "fetch";
+        fo = "fetch origin";
+        fu = "fetch upstream";
+        lease = "push --force-with-lease";
+        lol = "log --graph --decorate --pretty=oneline --abbrev-commit";
+        lola = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
+        pl = "pull";
+        pr = "pull -r";
+        ps = "push";
+        psf = "push -f";
+        rb = "rebase";
+        rbi = "rebase -i";
+        r = "remote";
+        ra = "remote add";
+        rr = "remote rm";
+        rv = "remote -v";
+        rs = "remote show";
+        st = "status";
+        stn = "status -uno";
       };
     };
     ignores = [
       "*~"
       "*.swp"
     ];
+  };
+
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      side-by-side = true;
+    };
   };
 
   programs.lazygit = {
