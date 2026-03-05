9fa2cf993dff2fa86b8008177640f32923b3eb9a
Cameron Smith <cameron.ray.smith@gmail.com>
2026-03-04 22:45:40 -0500

    refactor: wrap atuin config as flake-parts home-manager module

---
+++ modules/home/atuin.nix
@@ -0,0 +1,27 @@
+{ ... }:
+{
+  flake.modules.homeManager.atuin =
+    { lib, ... }:
+    {
+      xdg.configFile."atuin/config.toml".force = lib.mkForce true;
+      programs.atuin = {
+        enable = true;
+        enableZshIntegration = true;
+        settings = {
+          # TODO: enable s6 to manage atuin daemon
+          # for compatibility with ceph-filesystem
+          # https://forum.atuin.sh/t/weekly-release-2024-19/317
+          auto_sync = false;
+          ctrl_n_shortcuts = false;
+          keymap_mode = "vim-insert";
+          filter_mode_shell_up_key_binding = "directory";
+          search_mode = "fuzzy";
+          show_help = false;
+          show_preview = true;
+          daemon = {
+            enabled = true;
+          };
+        };
+      };
+    };
+}
