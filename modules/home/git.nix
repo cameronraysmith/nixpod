6f2143dd7ed399614d39b63af393f0df1a81789d
Cameron Smith <cameron.ray.smith@gmail.com>
2026-03-04 22:45:54 -0500

    refactor: wrap git config as flake-parts home-manager module

---
+++ modules/home/git.nix
@@ -0,0 +1,86 @@
+{ ... }:
+{
+  flake.modules.homeManager.git =
+    { ... }:
+    {
+      programs.git = {
+        enable = true;
+        settings = {
+          core.editor = "nvim";
+          credential.helper = "store --file ~/.git-credentials";
+          color.ui = true;
+          commit.gpgsign = false;
+          diff.colorMoved = "zebra";
+          fetch.prune = true;
+          format.signoff = true;
+          init.defaultBranch = "main";
+          merge.conflictstyle = "diff3";
+          push.autoSetupRemote = true;
+          rebase.autoStash = true;
+          alias = {
+            a = "add";
+            br = "branch";
+            bra = "branch -a";
+            c = "commit";
+            ca = "commit --amend";
+            can = "commit --amend --no-edit";
+            cavm = "commit -a -v -m";
+            cfg = "config --list";
+            cl = "clone";
+            cm = "commit -m";
+            co = "checkout";
+            cp = "cherry-pick";
+            cpx = "cherry-pick -x";
+            d = "diff";
+            f = "fetch";
+            fo = "fetch origin";
+            fu = "fetch upstream";
+            lease = "push --force-with-lease";
+            lol = "log --graph --decorate --pretty=oneline --abbrev-commit";
+            lola = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
+            pl = "pull";
+            pr = "pull -r";
+            ps = "push";
+            psf = "push -f";
+            rb = "rebase";
+            rbi = "rebase -i";
+            r = "remote";
+            ra = "remote add";
+            rr = "remote rm";
+            rv = "remote -v";
+            rs = "remote show";
+            st = "status";
+            stn = "status -uno";
+          };
+        };
+        ignores = [
+          "*~"
+          "*.swp"
+        ];
+      };
+
+      programs.delta = {
+        enable = true;
+        enableGitIntegration = true;
+        options = {
+          side-by-side = true;
+        };
+      };
+
+      programs.lazygit = {
+        enable = true;
+        settings = {
+          git = {
+            paging = {
+              colorArg = "always";
+              pager = "delta --color-only --dark --paging=never";
+              useConfig = false;
+            };
+            commit = {
+              signOff = true;
+            };
+          };
+        };
+      };
+    };
+}
