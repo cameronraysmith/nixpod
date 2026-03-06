ec4907991744aaeeddfe9c9dd080cb3572cbe13c
Cameron Smith <cameron.ray.smith@gmail.com>
2026-03-06 12:58:58 -0500

    refactor: expand nixConfig with comprehensive cache list synced from lib/caches.nix

--- flake.nix
+++ flake.nix
@@ -44,12 +44,21 @@
     systems.url = "github:nix-systems/default";
   };
 
+  # sync with lib/caches.nix
   nixConfig = {
-    extra-trusted-public-keys = [
-      "sciexp.cachix.org-1:HaliIGqJrFN7CDrzYVHqWS4uSISorWAY1bWNmNl8T08="
-    ];
     extra-substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+      "https://numtide.cachix.org"
+      "https://catppuccin.cachix.org"
       "https://sciexp.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
+      "sciexp.cachix.org-1:HaliIGqJrFN7CDrzYVHqWS4uSISorWAY1bWNmNl8T08="
     ];
   };
 }
