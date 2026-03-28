3f44ca2f704d822f14bfd5d737dba6bf466f77e4
Cameron Smith <cameron.ray.smith@gmail.com>
2026-03-28 01:01:52 -0400

    fix: update numtide cache url and public key in lib/caches.nix

--- lib/caches.nix
+++ lib/caches.nix
@@ -3,7 +3,7 @@
   substituters = [
     "https://cache.nixos.org"
     "https://nix-community.cachix.org"
-    "https://numtide.cachix.org"
+    "https://cache.numtide.com"
     "https://catppuccin.cachix.org"
     "https://sciexp.cachix.org"
   ];
@@ -11,7 +11,7 @@
   publicKeys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+    "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
     "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
     "sciexp.cachix.org-1:HaliIGqJrFN7CDrzYVHqWS4uSISorWAY1bWNmNl8T08="
   ];
